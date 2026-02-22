1d255222024693dfcba26ec10bee11daf27d054a
吴璇 <15487104+hswl-wu-xuan@user.noreply.gitee.com>
2026-02-22 14:36:50 +0800

    bug修复

--- config.py
+++ config.py
@@ -24,8 +24,8 @@
 import os
 
 DB_CONFIG = {
-    'host': os.getenv('MYSQL_HOST', 'localhost'),
-    'port': int(os.getenv('MYSQL_PORT', 3306)),
+    'host': os.getenv('MYSQL_HOST', '192.168.119.102'),
+    'port': int(os.getenv('MYSQL_PORT', 3308)),   # ← 改成 3308
     'user': os.getenv('MYSQL_USER', 'root'),
     'password': os.getenv('MYSQL_PASSWORD', '123456'),
     'database': os.getenv('MYSQL_DATABASE', 'plagiarism_db'),
